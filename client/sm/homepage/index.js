394d1819176ec90e4059887dea80ba033305453a
Nathan Stitt <nathan@stitt.org>
2017-04-29 22:28:06 -0500

    update homepage

--- client/sm/homepage/index.js
+++ client/sm/homepage/index.js
@@ -1,1 +1,8 @@
+import whenDomReady from 'when-dom-ready';
+import SmoothScroll from 'lanes/lib/smooth-scroll';
 import './homepage.scss';
+
+whenDomReady(() => {
+    document.querySelectorAll('.signup-link')
+        .forEach(link => new SmoothScroll(link, '#signup'));
+});
