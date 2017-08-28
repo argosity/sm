de7e4159e219f2af01c14f5f07ac03df9daa0992
Nathan Stitt <nathan@stitt.org>
2017-08-27 22:19:41 -0500

    add an icon for menu options

--- config/screens.rb
+++ config/screens.rb
@@ -11,41 +11,42 @@
 Hippo::Screen.for_extension 'sm' do | screens |
     screens.define "box-office" do | screen |
         screen.title       = "Box Office"
-        screen.description = ""
-        screen.icon        = ""
+        screen.icon        = "ticket"
         screen.group_id    = ""
     end
 
     screens.define "events" do | screen |
         screen.title       = "Events"
         screen.description = ""
-        screen.icon        = ""
+        screen.icon        = "id-card-o"
         screen.group_id    = ""
     end
     screens.define "presenters" do | screen |
         screen.title       = "Presenters"
         screen.description = ""
-        screen.icon        = ""
+        screen.icon        = "magic"
         screen.group_id    = ""
     end
     screens.define "venues" do | screen |
         screen.title       = "Venues"
         screen.description = ""
-        screen.icon        = ""
+        screen.icon        = "building-o"
         screen.group_id    = ""
     end
     screens.define "embeds" do | screen |
         screen.title       = "Embedding"
         screen.description = ""
-        screen.icon        = ""
+        screen.icon        = "list-alt"
         screen.group_id    = ""
     end
 
     screens.extend 'user-management' do | screen |
         screen.group_id = ''
+        screen.icon        = "users"
     end
     screens.extend 'system-settings' do | screen |
         screen.group_id = ''
+        screen.icon        = "cogs"
     end
 
 end
