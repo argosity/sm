cd965e88ab725e1295501cd4c11c77eb21139e4a
Nathan Stitt <nathan@stitt.org>
2017-09-09 15:26:22 -0500

    set sales halt time

--- spec/factories/venue.rb
+++ spec/factories/venue.rb
@@ -5,6 +5,7 @@ FactoryGirl.define do
         phone_number { Faker::PhoneNumber.phone_number }
         capacity { (rand*100).to_i }
         timezone { ActiveSupport::TimeZone::MAPPING.values.sample }
+        online_sales_halt_mins_before  { (rand*90).to_i + 30 }
         address do
             "#{Faker::Address.street_address} #{Faker::Address.city} #{Faker::Address.state}"
         end
