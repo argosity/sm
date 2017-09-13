8f93339195c09fec8b93cdbd2f871a65ccb7f3ac
Nathan Stitt <nathan@stitt.org>
2017-09-12 21:29:49 -0500

    Save when save clicked, add cancel button

--- client/sm/screens/shows/page-editor.jsx
+++ client/sm/screens/shows/page-editor.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { action } from 'mobx';
+import { action, observable } from 'mobx';
 import { observer } from 'mobx-react';
-
 import Box from 'grommet/components/Box';
 import Layer from 'grommet/components/Layer';
+import Button from 'grommet/components/Button';
+import RevertIcon from 'grommet/components/icons/base/Revert';
+import SaveIcon from 'grommet/components/icons/base/Save';
 
 import NetworkActivityOverlay from 'hippo/components/network-activity-overlay';
-
 import TextEditor from 'hippo/components/text-editor';
 
 import Show from '../../models/show';
@@ -22,10 +23,15 @@ class PageEditor extends React.PureComponent {
         onComplete: PropTypes.func.isRequired,
     }
 
-    @action.bound
-    onDone({ content }) {
-        this.props.show.set({ page: content });
-        this.props.onComplete();
+    @observable content;
+
+    @action.bound onChange(content) {
+        this.content = content;
+    }
+
+    @action.bound onSave() {
+        this.props.show.set({ page: this.content });
+        this.props.show.save().then(this.props.onComplete);
     }
 
     render() {
@@ -41,8 +47,15 @@ class PageEditor extends React.PureComponent {
                     <TextEditor
                         defaultContent={show.page}
                         assets={show.page_images}
-                        onComplete={this.onDone}
-                    />
+                        onChange={this.onChange}
+                    >
+                        <Button
+                            style={{ order: -2 }}
+                            plain icon={<RevertIcon />} label="Cancel" onClick={onComplete}
+                        />
+                        <span style={{ order: -1, flex: 1 }} />
+                        <Button plain icon={<SaveIcon />} label="Save" onClick={this.onSave} />
+                    </TextEditor>
                 </Box>
             </Layer>
         );
