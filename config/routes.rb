5e381cf6d3306ef98a5ac5daa8b58ec6584a6b5c
Nathan Stitt <nathan@stitt.org>
2017-09-15 15:32:07 -0500

    allow fetching payment token with CORS

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Hippo::API.routes.for_extension 'sm' do
     # Embed routes must come before the Embed resource, otherwise it handles the requests
     resources SM::ShowTime
     resources SM::Sale, controller: SM::Handlers::Sale, cors: '*', public: true
-    resources SM::Payment, controller: SM::Handlers::Payment
+    resources SM::Payment, controller: SM::Handlers::Payment, cors: '*', public: true
     resources SM::Show, path: 'embed/shows', controller: SM::Handlers::Shows, cors: '*', public: true
 
     resources SM::Show
