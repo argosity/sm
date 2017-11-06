b38b04c732f474d8132c89366d97d67add841f0c
Nathan Stitt <nathan@stitt.org>
2017-11-06 10:30:42 -0600

    lock time when testing

--- spec/client/screens/box-office.spec.jsx
+++ spec/client/screens/box-office.spec.jsx
@@ -1,10 +1,17 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
 import { Snapshot, getScreenInstance } from 'hippo/testing/index';
 import BoxOffice from 'sm/screens/box-office';
+import chronokinesis from 'chronokinesis';
+import moment from 'moment-timezone';
 
 const screenInstance = getScreenInstance('box-office');
 
 describe('Screen Gate', () => {
+    beforeEach(() => {
+        chronokinesis.travel(new Date('2017-05-01T21:00:00.000Z'));
+        moment.tz.setDefault('America/Los_Angeles');
+    });
+
     it('renders and matches snapshot', () => {
         expect(Snapshot(<BoxOffice screen={screenInstance} />)).toMatchSnapshot();
     });
