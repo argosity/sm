86f0675c292c806d3b43bed65cb4ede83bd3a642
Nathan Stitt <nathan@stitt.org>
2017-11-13 21:27:20 -0600

    convert to use momentjs for dates

--- client/sm/models/show-time.js
+++ client/sm/models/show-time.js
@@ -19,11 +19,11 @@ export default class ShowTime extends BaseModel {
 
     @field price;
     @field capacity;
-    @field occurs_at; // ({ type: 'date' }) occurs_at = moment()
-    // .startOf('day')
-    // .add(8, 'hour')
-    // .add(1, 'week')
-    // .toDate();
+
+    @field({ type: 'date' }) occurs_at = moment()
+        .startOf('day')
+        .add(8, 'hour')
+        .add(1, 'week')
 
     constructor(attrs) {
         super(attrs);
