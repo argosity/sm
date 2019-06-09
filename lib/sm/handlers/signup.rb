68bf490f800f7de28b799d486bdc031fe22e131f
Nathan Stitt <nathan@stitt.org>
2019-06-09 13:34:41 -0500

    suspend signup due to spam

--- lib/sm/handlers/signup.rb
+++ lib/sm/handlers/signup.rb
@@ -5,26 +5,26 @@ module SM::Handlers
         def self.handler
             lambda do
 
-                tenant = Hippo::Tenant.new(params.slice(:email))
-                Hippo::Tenant.transaction do
-                    tenant.name = params['company']
-                    tenant.perform do
-                        user = tenant.users.build(params.slice(:name, :email, :login, :password)
-                                               .merge(role_names: ["administrator"]))
-                        if tenant.save
-                            # Hippo::Tenant.system.perform do
-                            #   SM::Templates::Signup.create(tenant, user).deliver
-                            # end
-                        end
-                    end
-                end
-                if tenant.errors.any?
-                    erb :signup, locals: { tenant: tenant }
-                else
-                    tenant.perform do
-                        erb :signup_success, locals: { tenant: tenant }
-                    end
-                end
+                # tenant = Hippo::Tenant.new(params.slice(:email))
+                # Hippo::Tenant.transaction do
+                #     tenant.name = params['company']
+                #     tenant.perform do
+                #         user = tenant.users.build(params.slice(:name, :email, :login, :password)
+                #                                .merge(role_names: ["administrator"]))
+                #         if tenant.save
+                #             # Hippo::Tenant.system.perform do
+                #             #   SM::Templates::Signup.create(tenant, user).deliver
+                #             # end
+                #         end
+                #     end
+                # end
+                # if tenant.errors.any?
+                #     erb :signup, locals: { tenant: tenant }
+                # else
+                #     tenant.perform do
+                #         erb :signup_success, locals: { tenant: tenant }
+                #     end
+                # end
             end
         end
     end
